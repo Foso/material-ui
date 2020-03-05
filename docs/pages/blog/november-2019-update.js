8618d36ad5b396920dafbd170b85374f9484f59d
Jens Klingenberg <mail@jensklingenberg.de>
2020-03-05 19:42:45 +0100

    change folders

--- docs/pages/blog/november-2019-update.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import TopLayoutBlog from 'docs/src/modules/components/TopLayoutBlog';
-import markdown from './november-2019-update.md';
-
-export default function Page() {
-  return <TopLayoutBlog markdown={markdown} />;
-}
--- docs/pages/blog/september-2019-update.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import TopLayoutBlog from 'docs/src/modules/components/TopLayoutBlog';
-import markdown from './september-2019-update.md';
-
-export default function Page() {
-  return <TopLayoutBlog markdown={markdown} />;
-}
